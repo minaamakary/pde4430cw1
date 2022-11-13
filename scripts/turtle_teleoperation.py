39ddd40e1fabf1c53a2a91e8ff7354b13d1c330c
minamakary <mina.makary@live.com>
2022-11-14 00:51:44 +0400

    now the turtle is moving when we press the keyboard, ex1a should be done at this point

--- scripts/turtle_teleoperation.py
+++ scripts/turtle_teleoperation.py
@@ -1,4 +1,4 @@
-#!/usr/bin/ env python3
+#!/usr/bin/env python3
 import rospy
 import getch
 from geometry_msgs.msg import Twist
@@ -6,38 +6,39 @@ from geometry_msgs.msg import Twist
 #This is my node
 
 def initialise():
-	rospy.init_node('turtle_teleoperation.py', annoymous=True) # init node here
-	mypublisher=rospy.publisher('/turtle1/cmd_vel', Twist, queue_size=10) # init publisher here
-	rospy.rate(10)
+	rospy.init_node('turtle_teleoperation.py', anonymous=False) # init node here
+	pub=rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10) # init publisher here
+	rate=rospy.Rate(50)
 	move_message = Twist()
 	while not rospy.is_shutdown():
-	key = ord(getch.getch())
-	if key==65:
-		rospy.loginfo("Up key pressed")
-		move_message.linear.x = 1.0
-		move_message.angular.z = 0.0
-		pub.publish(move_message)
+		key = ord(getch.getch())
+		if key==65:
+			rospy.loginfo("Up key pressed")
+			move_message.linear.x = 1.0
+			move_message.angular.z = 0.0
+			pub.publish(move_message)
 		# move turtle forward here
-	elif key==66:
-		rospy.loginfo("Down key pressed")
-		move_message.linear.x = -1.0
-		move_message.angular.z = 0.0
-		pub.publish(move_message)
+		elif key==66:
+			rospy.loginfo("Down key pressed")
+			move_message.linear.x = -1.0
+			move_message.angular.z = 0.0
+			pub.publish(move_message)
 		# move turtle backwards here
-	elif key==67:
-		move_message.linear.x = 0.0
-		move_message.angular.z = -1.0
-		pub.publish(move_message)
-		rospy.loginfo("left key pressed")
+		elif key==67:
+			move_message.linear.x = 0.0
+			move_message.angular.z = 1.0
+			pub.publish(move_message)
+			rospy.loginfo("left key pressed")
 		#move turtle left here
 
-	elif key=68:
-		rospy.loginfo("right key pressed")
-		move_message.linear.x = 0.0
-		move_message.angular.z = 1.0
-		pub.publish(move_message)
+		elif key==68:
+			rospy.loginfo("right key pressed")
+			move_message.linear.x = 0.0
+			move_message.angular.z = -1.0
+			pub.publish(move_message)
 		#move turtle right here
 
+		rate.sleep()
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
 	initialise()
